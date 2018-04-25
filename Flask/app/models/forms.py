9dcee2ba4f38871fae60573f94e83569bad01326
RodrigoMarquesCampos <digoleeke@gmail.com>
2018-04-25 17:58:32 -0300

    Começando formulários

--- Flask/app/models/forms.py
+++ Flask/app/models/forms.py
@@ -6,3 +6,10 @@ class LoginForm(FlaskForm):
     username = StringField("username", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
     remember_me = BooleanField("remember_me")
+
+class UsuarioForm(FlaskForm):
+    nome = StringField("username", validators=[DataRequired()])
+    cargo  = StringField("username", validators=[DataRequired()])
+    rg = StringField("username", validators=[DataRequired()])
+    cpf = StringField("username", validators=[DataRequired()])
+    rua = StringField("username", validators=[DataRequired()])
