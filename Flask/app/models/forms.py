3f9f69620adcc94d5eba29dbf41351701afa9527
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-04-25 19:57:42 -0300

    Formularios Usuario, Professor e Desciplina criados

--- Flask/app/models/forms.py
+++ Flask/app/models/forms.py
@@ -2,14 +2,25 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()])
-    remember_me = BooleanField("remember_me")
-
 class UsuarioForm(FlaskForm):
-    nome = StringField("username", validators=[DataRequired()])
-    cargo  = StringField("username", validators=[DataRequired()])
-    rg = StringField("username", validators=[DataRequired()])
-    cpf = StringField("username", validators=[DataRequired()])
-    rua = StringField("username", validators=[DataRequired()])
+    nome = StringField("nome", validators=[DataRequired()])
+    cargo = StringField("cargo", validators=[DataRequired()])
+    rg = StringField("rg", validators=[DataRequired()])
+    cpf = StringField("cpf", validators=[DataRequired()])
+    rua = StringField("rua", validators=[DataRequired()])
+    numero = StringField("numero", validators=[DataRequired()])
+    bairro = StringField("bairro", validators=[DataRequired()])
+    login = StringField("login", validators=[DataRequired()])
+    senha = StringField("senha", validators=[DataRequired()])
+
+
+class ProfessorForm(FlaskForm):
+    institucional = StringField("institucional", validators=[DataRequired()])
+    area = StringField("area", validators=[DataRequired()])
+    usuario = StringField("usuario", validators=[DataRequired()])
+
+
+class DisciplinaForm(FlaskForm):
+    nome = StringField("nome", validators=[DataRequired()])
+    descricao = StringField("descricao", validators=[DataRequired()])
+    professor = StringField("professor", validators=[DataRequired()])
