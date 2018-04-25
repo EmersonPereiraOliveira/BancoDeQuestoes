38a9b51522b7fd9a5fcdfd04c7ca8bbb6197965a
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-04-25 20:37:23 -0300

    Form usuario funcionando

--- Flask/app/controllers/default_routes.py
+++ Flask/app/controllers/default_routes.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, url_for, request
 from app import app, db
 
+from app.models.forms import UsuarioForm, ProfessorForm
+from app.models.tables import Usuario, Professor
+
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
     return "Pronto! Servidor OK"
+
+
+@app.route("/cadastrar-professor")
+def cadastrar_professor():
+    form_usuario = UsuarioForm()
+    form_professor = ProfessorForm()
+    data = [form_usuario, form_professor]
+    return render_template("cadastrar-professor.html", form_usuario=form_usuario)
