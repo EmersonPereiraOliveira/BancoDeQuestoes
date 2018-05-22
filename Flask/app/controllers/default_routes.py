4e5ab1092a5ad2e64859149d8e4fd80ef284afd6
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-05-22 20:33:32 -0300

    Add valor no formulário do template ok!

--- Flask/app/controllers/default_routes.py
+++ Flask/app/controllers/default_routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, flash, url_for, redirect
 from app import app, db
 
-from app.models.forms import UsuarioForm, ProfessorForm
+from app.models.forms import UsuarioForm, ProfessorForm, CrudProfessorForm
 from app.models.tables import Usuario, Professor
 
 @app.route("/")
@@ -42,8 +42,97 @@ def cadastrar_professor():
             flash("Cadastro de usuário realizado com sucesso!")
             return redirect(url_for('cadastrar_professor'))
 
-
     form_usuario = UsuarioForm()
     form_professor = ProfessorForm()
     data = [form_usuario, form_professor]
     return render_template("cadastrar-professor.html", data=data)
+
+@app.route("/editar-professor/<int:id>", methods=["GET","POST"])
+def editar_professor():
+    professor = db.session.query(Professor).filter_by(_id=id).first
+
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        cargo = request.form.get("cargo")
+        cpf = request.form.get("cpf")
+        rg = request.form.get("rg")
+        rua = request.form.get("rua")
+        numero = request.form.get("numero")
+        bairro = request.form.get("bairro")
+        login = request.form.get("login")
+        senha = request.form.get("senha")
+        # Professor
+        #area = request.form.get("area")
+        #institucional = request.form.get("institucional")
+
+        if nome and cargo and cpf and rg and rua and numero and bairro and login and senha :
+            professor.nome = nome
+            professor.cargo = cargo
+            professor.cpf = cpf
+            professor.rg = rg
+            professor.rua = rua
+            professor.numero = numero
+            professor.bairro = bairro
+            professor.login = login
+            professor.senha = senha
+
+            db.session.commit
+            return redirect(url_for(editar_professor))
+
+        return render_template("editar_professor",data=data)
+    form_professor = ProfessorForm()
+
+
+@app.route("/buscar-professor", methods=["GET", "POST"])
+def buscar_professor():
+    #usuario = db.session.query(Usuario).filter_by().first
+    form_professor = ProfessorForm()
+    professor = db.session.query(Professor).all()
+    data = [form_professor, professor]
+    return render_template("buscar-professor.html", data=data)
+
+
+
+@app.route("/crud-professor", methods=["GET","POST"])
+def crud_professor():
+    form_crud_professor = CrudProfessorForm()
+    #usuario = db.session.query(Usuario).all()
+    usuario = db.session.query(Usuario).first()
+    professor = db.session.query(Professor).all()
+    data = [form_crud_professor, usuario, professor]
+    return render_template("crud-professor.html", data=data)
+
+
+
+@app.route("/crud-professor-executar/<int:id>", methods=["GET","POST"])
+def crud_professor_executar():
+    professor = db.session.query(Professor).filter_by(_id=id).first
+
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        cargo = request.form.get("cargo")
+        cpf = request.form.get("cpf")
+        rg = request.form.get("rg")
+        rua = request.form.get("rua")
+        numero = request.form.get("numero")
+        bairro = request.form.get("bairro")
+        login = request.form.get("login")
+        senha = request.form.get("senha")
+
+
+        if nome and cargo and cpf and rg and rua and numero and bairro and login and senha :
+            professor.nome = nome
+            professor.cargo = cargo
+            professor.cpf = cpf
+            professor.rg = rg
+            professor.rua = rua
+            professor.numero = numero
+            professor.bairro = bairro
+            professor.login = login
+            professor.senha = senha
+
+            db.session.commit
+            return redirect(url_for(editar_professor))
+
+        return render_template("editar_professor",data=data)
+    form_professor = ProfessorForm()
--- Flask/app/models/forms.py
+++ Flask/app/models/forms.py
@@ -24,3 +24,19 @@ class DisciplinaForm(FlaskForm):
     nome = StringField("nome", validators=[DataRequired()])
     descricao = StringField("descricao", validators=[DataRequired()])
     professor = StringField("professor", validators=[DataRequired()])
+
+class CrudProfessorForm(FlaskForm):
+    nome = StringField("nome", validators=[DataRequired()])
+    cargo = StringField("cargo", validators=[DataRequired()])
+    rg = StringField("rg", validators=[DataRequired()])
+    cpf = StringField("cpf", validators=[DataRequired()])
+    rua = StringField("rua", validators=[DataRequired()])
+    numero = StringField("numero", validators=[DataRequired()])
+    bairro = StringField("bairro", validators=[DataRequired()])
+    login = StringField("login", validators=[DataRequired()])
+    senha = StringField("senha", validators=[DataRequired()])
+    institucional = StringField("institucional", validators=[DataRequired()])
+    area = StringField("area", validators=[DataRequired()])
+    usuario = StringField("usuario", validators=[DataRequired()])
+
+
